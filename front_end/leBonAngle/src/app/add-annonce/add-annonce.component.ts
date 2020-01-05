4d544e816dd8003c81dba1c6804fa0b1a5e04760
nasd4q <nasd4q@gmail.com>
2020-01-05 18:24:07 +0100

    f-e going to create addAnnonceCompo 2

--- front_end/leBonAngle/src/app/add-annonce/add-annonce.component.ts
+++ front_end/leBonAngle/src/app/add-annonce/add-annonce.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Categorie } from 'src/model/categorie';
+import { Utilisateur } from 'src/model/utilisateur';
+import { DataService } from 'src/service/data.service';
 
 @Component({
   selector: 'app-add-annonce',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-annonce.component.scss']
 })
 export class AddAnnonceComponent implements OnInit {
+  categories: Categorie[];
+  auteurs: Utilisateur[];
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+  }
+
+  loadCategories() {
+    this.dataService.getCategories().then(
+      categories => {
+        this.categories = categories;
+      }
+    );
+  }
+
+  loadAuteurs() {
+    this.dataService.getUtilisateurs().then(
+      utilisateurs => {
+        this.auteurs = utilisateurs;
+      }
+    );
   }
 
 }
