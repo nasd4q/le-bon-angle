3a7222940ce92dfe07a054c5567d8ee7bb6e5ac8
apple <apple@MacBook-Pro-de-apple.local>
2020-06-19 15:42:34 +0200

    latest

--- back_end/leBonAngleAPI/src/test/java/com/nasd4q/leBonAngleAPI/LeBonAngleApiApplicationTests.java
+++ back_end/leBonAngleAPI/src/test/java/com/nasd4q/leBonAngleAPI/LeBonAngleApiApplicationTests.java
@@ -33,25 +33,28 @@ class LeBonAngleApiApplicationTests {
 
 	@Test
 	public void testAnnonceRepositoryFindByX() {
-		Utilisateur u1 = new Utilisateur("jack", "0765234610", "jack@ctu.com");
-		Utilisateur u2 = new Utilisateur("chloe", "0765234623", "chloe@ctu.com");
+		Utilisateur u1 = new Utilisateur("Jack", "0765234610", "jack@ctu.com");
+		Utilisateur u2 = new Utilisateur("Chloe", "0765234623", "chloe@ctu.com");
 
 		u1 = utilisateurRepository.save(u1);
 		u2 = utilisateurRepository.save(u2);
 
-		Categorie c1 = new Categorie("armes");
-		Categorie c2 = new Categorie("comms units");
+		Categorie c1 = new Categorie("Téléphonie");
+		Categorie c2 = new Categorie("Habillement");
 
 		c1 = categorieRepository.save(c1);
 		c2 = categorieRepository.save(c2);
 
-		Annonce a1 = new Annonce("Vend ak47", "AK 47, bon etat, tres peu servi", new BigDecimal(99.99), "LA, CA", u1,
+		Annonce a1 = new Annonce("Vend Galaxy S10", "Samsung Galaxy S10, bon état, très peu servi", new BigDecimal(499.99), "LA, CA", u1,
 				c1);
-		Annonce a2 = new Annonce("Vend telephone", "Samsung S10, bon etat, tres peu servi", new BigDecimal(199.99),
+		Annonce a2 = new Annonce("Vend Iphone X", "Iphone X, bon état, très peu servi", new BigDecimal(199.99),
+				"LA, CA", u2, c1);
+		Annonce a3 = new Annonce("Vend Casquette", "Casquette officielle des Pirates de Pittsburgh", new BigDecimal(99.99),
 				"LA, CA", u2, c2);
 
 		a1 = annonceRepository.save(a1);
 		a2 = annonceRepository.save(a2);
+		a3 = annonceRepository.save(a3);
 
 		List<Annonce> l = annonceRepository.findByAuteur_Id(u1.getId());
 
@@ -67,6 +70,7 @@ class LeBonAngleApiApplicationTests {
 
 		annonceRepository.delete(a1);
 		annonceRepository.delete(a2);
+		annonceRepository.delete(a3);
 
 	}
 
